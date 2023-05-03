c420303655e8ffde2494b061418d0fb85283a833
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-03 10:52:47 +0530

    learn-terraform

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -30,6 +30,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "frontend-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -38,6 +46,15 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "mongodb-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -46,6 +63,15 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "catalogue-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -54,6 +80,16 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "user-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
+
+
 resource "aws_instance" "redis" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -61,6 +97,14 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "redis-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
 }
 resource "aws_instance" "cart" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
@@ -70,6 +114,14 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "cart-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
 resource "aws_instance" "mysql" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -78,6 +130,15 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "mysql-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -85,6 +146,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "shipping-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
 }
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
@@ -95,6 +164,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "rabbitmq-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -102,4 +179,12 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z0299491JAQ87HTY8OKC"
+  name    = "payment-dev.cskvsmi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.HelloWorld.private_ip]
 }
